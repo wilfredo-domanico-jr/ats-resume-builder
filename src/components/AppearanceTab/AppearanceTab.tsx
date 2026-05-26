747a5edafaa6cd78e0c32b5d853cea2094c874c5
Wilfredo Domanico <wilfredo.domanico.jr@gmail.com>
2026-05-26 10:59:34 +0800

    Remove Font size adjustment

--- src/components/AppearanceTab/AppearanceTab.tsx
+++ src/components/AppearanceTab/AppearanceTab.tsx
@@ -14,8 +14,6 @@ function AppearanceTab({
   resumeTheme,
   setResumeTheme,
 }: AppearanceTabProps) {
-  const [fontSize, setFontSize] = useState(13);
-
   useEffect(() => {
     if (resumeTheme) {
       document.documentElement.style.setProperty(
@@ -28,14 +26,6 @@ function AppearanceTab({
       );
     }
   }, [resumeTheme]);
-
-  const updateFontSize = (value: number) => {
-    setFontSize(value);
-    document.documentElement.style.setProperty(
-      "--resume-font-size",
-      `${value}px`,
-    );
-  };
 
   return (
     <div className="form-panel-inner">
@@ -51,38 +41,6 @@ function AppearanceTab({
             onClick={() => setResumeTheme(t)}
           />
         ))}
-      </div>
-
-      <hr />
-
-      <div className="section-title">Font Size</div>
-
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "0.75rem",
-          marginTop: "0.25rem",
-        }}
-      >
-        <input
-          type="range"
-          min={11}
-          max={16}
-          value={fontSize}
-          onChange={(e) => updateFontSize(Number(e.target.value))}
-          style={{ width: "100%" }}
-        />
-
-        <span
-          style={{
-            fontSize: "0.8rem",
-            color: "var(--text-2)",
-            minWidth: "28px",
-          }}
-        >
-          {fontSize}px
-        </span>
       </div>
 
       <hr />
